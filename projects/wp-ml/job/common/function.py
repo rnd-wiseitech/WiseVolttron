d12a5b2d074f4fb61d76485cbcc4159cdc513244
smhan <lion3471@gmail.com>
2025-12-15 09:37:37 +0900

    volttron 실시간 데이터 수신 기능 추가

--- projects/wp-ml/job/common/function.py
+++ projects/wp-ml/job/common/function.py
@@ -1,6 +1,11 @@
 import pandas as pd
 from deltalake import write_deltalake, DeltaTable
 import pyarrow as pa
+from sqlalchemy import text
+import json
+import subprocess
+import sys
+from database.manager import WpDataBaseManagement
 
 def execute(p_dataSource, **kwargs):
     # 기능이 필요는 한데, 파일로 따로 관리하기는 애매한 기능들 모아놓은 파일
@@ -18,6 +23,45 @@ def execute(p_dataSource, **kwargs):
             s_result = {'result' : True}
         else :
             s_result = {'result' : False, 'useColumn':s_datasetColumn}
+
+     # 토픽 전달
+    if s_functionType =='topicSend':
+        s_topic = kwargs['topic']
+        
+        # 여기 토픽을 volttron 워크플로우 중에서 있는것만 찾아 실행시켜야한다 
+        s_dbMng = WpDataBaseManagement('meta')
+        s_volttronQuery = text(""" SELECT 
+                            A.SCH_ID,
+                            B.WF_NM,
+                            B.WF_ID,
+                            B.REG_USER,
+                            B.WF_PATH,
+                            E.WF_DATA
+                        FROM WK_SCH_MSTR A
+                        INNER JOIN WF_MSTR B ON A.WF_ID = B.WF_ID
+                        LEFT OUTER JOIN WF_USE_DATASET C ON B.WF_ID = C.WF_ID 
+                        LEFT OUTER JOIN DS_VIEW_MSTR D ON C.DS_VIEW_ID = D.DS_VIEW_ID
+                        INNER JOIN WF_COM_MSTR E ON A.WF_ID = E.WF_ID
+                        WHERE A.DEL_YN='N' AND A.SCH_STATUS LIKE 'REALVOLTTRON_20' AND E.COM_ID = 78 GROUP BY SCH_ID ORDER BY SCH_ID DESC;""")
+        s_workflow = s_dbMng.o_conn.execute(s_volttronQuery).mappings().all()
+
+        # 여기서 log_id랑 sch_id 같이 보내서 장치관리 로그 만들기
+        
+        for s_component in s_workflow:
+            s_componentTopic = json.loads(s_component['WF_DATA'])['wp-data']['o_data']['topic']
+            if s_topic == s_componentTopic:
+                s_sch_id = s_component['SCH_ID']
+                
+                try :
+                    s_param = { "topic" : s_topic, "offset" :  kwargs['offset'], "sch_id": s_sch_id}
+                    s_execPath = s_component["WF_PATH"]
+                    s_run = subprocess.Popen([sys.executable, s_execPath, json.dumps(s_param)], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stdin=subprocess.DEVNULL, start_new_session=True)
+                    s_run.wait()
+                    
+                except Exception as e:
+                    pass
+                    
+        s_result = True
 
     if s_functionType =='writeDeltaLake':
         s_mode = 'overwrite'
