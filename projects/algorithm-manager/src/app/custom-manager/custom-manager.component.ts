b0d0579cb76eeaac08b0e572040f714fa4a5e08e
smhan <lion3471@gmail.com>
2025-12-18 14:45:18 +0900

    미사용 코드 제거

--- projects/algorithm-manager/src/app/custom-manager/custom-manager.component.ts
+++ projects/algorithm-manager/src/app/custom-manager/custom-manager.component.ts
@@ -37,8 +37,6 @@ export class CustomManagerComponent implements OnInit {
     '알고리즘 타입',
     '모델 파일명',
     '사용 스케일러',
-    // '인코더 파일명',
-    // '예시 데이터명',
     '소유자',
     '등록일'
   ];
@@ -49,8 +47,6 @@ export class CustomManagerComponent implements OnInit {
     'ARG_TYPE',
     'MODEL_FILE',
     'SCALE_FILE',
-    // 'ENCODER_FILE',
-    // 'EXAMPLE_FILE',
     'USER_ID',
     'REG_DATE'
   ];
@@ -66,16 +62,16 @@ export class CustomManagerComponent implements OnInit {
   oServerURL = this.cAppConfig.getServerPath("NODE");
 
   
-  uploadStatus: string | null = null; // 업로드 상태 메시지
+  uploadStatus: string | null = null;
 
-  h_uploadProgress: number | null = null; // 업로드 진행률
-  h_isUploading: boolean = false; // 업로드 중 여부
+  h_uploadProgress: number | null = null;
+  h_isUploading: boolean = false;
   h_uploadFile = '';
   h_uploadStatus = '업로드';
   h_currentFileSize = '0B';
   h_totalFileSize = '0B';
 
-  private o_uploadSubscription: Subscription | null = null; // 업로드 Subscription
+  private o_uploadSubscription: Subscription | null = null;
 
   constructor(public cDialog: MatDialog,
     private cMainAppSvc: MainAppService,
@@ -87,7 +83,6 @@ export class CustomManagerComponent implements OnInit {
   ) { }
 
   ngOnDestroy(): void {
-    // this.oSocketSubscribe.unsubscribe();
   }
   ngOnInit(): void {
 
@@ -110,9 +105,9 @@ export class CustomManagerComponent implements OnInit {
     }, {
       vname: '프레임워크 타입',
       name: 'FRAMEWORK_TYPE',
-      value: ['Scikit-learn', 'PyTorch', 'TensorFlow/Keras'],
+      value: ['Scikit-learn', 'PyTorch'],
       type: 'select',
-      fvalue: ['Scikit-learn', 'PyTorch', 'TensorFlow/Keras'],
+      fvalue: ['Scikit-learn', 'PyTorch'],
       visible: true,
       edit: true,
       callbak: {name:'selectFrameWork'},
@@ -120,9 +115,9 @@ export class CustomManagerComponent implements OnInit {
     }, {
       vname: '알고리즘 타입',
       name: 'ARG_TYPE',
-      value: ['Classification', 'Regression', 'Clustering', 'Reinforcement', 'LSTM'],
+      value: ['Classification', 'Regression', 'Clustering'],
       type: 'select',
-      fvalue: ['Classification', 'Regression', 'Clustering', 'Reinforcement', 'LSTM'],
+      fvalue: ['Classification', 'Regression', 'Clustering'],
       visible: true,
       edit: true,
       callbak: null,
@@ -170,77 +165,6 @@ export class CustomManagerComponent implements OnInit {
       callbak: {name:'openPythonCode'},
       validation: false
   }
-      // {
-      //   vname: '접속 확인',
-      //   name: 'CONNECT_STATUS',
-      //   value: '',
-      //   type: 'button',
-      //   fvalue: null,
-      //   visible: true,
-      //   edit: true,
-      //   callbak:{name:'onServerCheck'},
-      //   validation: true
-      // }, {
-      //   vname: '호스트명',
-      //   name: 'HOSTNAME',
-      //   value: '',
-      //   type: 'text',
-      //   fvalue: null,
-      //   visible: true,
-      //   edit: false,
-      //   callbak: null,
-      //   validation: false
-      // }, {
-      //   vname: 'OS명',
-      //   name: 'OS_VERSION',
-      //   value: '',
-      //   type: 'text',
-      //   fvalue: null,
-      //   visible: true,
-      //   edit: false,
-      //   callbak: null,
-      //   validation: false
-      // }, {
-      //   vname: '총 코어',
-      //   name: 'CORE',
-      //   value: '',
-      //   type: 'number',
-      //   fvalue: '',
-      //   visible: true,
-      //   edit: false,
-      //   callbak: null,
-      //   validation: false
-      // }, {
-      //   vname: '총 메모리(GB)',
-      //   name: 'MEMORY_GB',
-      //   value: '',
-      //   type: 'number',
-      //   fvalue: '',
-      //   visible: true,
-      //   edit: false,
-      //   callbak: null,
-      //   validation: false
-      // }, {
-      //   vname: '총 디스크(GB)',
-      //   name: 'HDD_GB',
-      //   value: '',
-      //   type: 'number',
-      //   fvalue: '',
-      //   visible: true,
-      //   edit: false,
-      //   callbak: null,
-      //   validation: false
-      // }, {
-      //   vname: '서버 설명',
-      //   name: 'SERVER_DESC',
-      //   value: null,
-      //   type: 'text-area',
-      //   fvalue: null,
-      //   visible: true,
-      //   edit: false,
-      //   callbak: null,
-      //   validation: false
-      // }
     ];
     this.oWpPopData = {
       'title': '커스텀 모델 업로드',
@@ -270,7 +194,6 @@ export class CustomManagerComponent implements OnInit {
           this.oServerList.push((pData.result[sIdx].MODEL_NM).toLowerCase());
         }
         for (let sCol of Object.keys(pData.result[0])) {
-          // this.oServerList.push((pData[sIdx].DS_VIEW_NM).toLowerCase());
           let sIndex = this.oDisplayedColumns.findIndex(pVal => pVal === sCol);
           if (sIndex == -1) {
             sColInfo.push({
@@ -297,54 +220,8 @@ export class CustomManagerComponent implements OnInit {
         });
         this.oGridCol = sColInfo;
       }
-      // console.log("this.oGridCol : ", this.oGridCol);
       this.cWpLibSvc.showProgress(false, "wdspin");
     });
-  }
-
-
-
-
-
-  connectSeleted(pDsData: any) {
-    // if (pDsData != undefined) {
-    // this.oSelectedDs = pDsData;
-    //   if(this.o_SelectedFormData.CONNECTION_TYPE != 'db'){
-    //     this.resetSelectedForm(this.o_SelectedFormData.CONNECTION_TYPE);
-
-    //     const dialogRef = this.cDialog.open(DmHdfsPopUpComponent, {data:pDsData.DS_ID});
-    //     dialogRef.afterClosed().subscribe(pRes => {
-    //       if (pRes) {
-    //         if (pRes.result) {
-    //           let sResult = pRes.data;
-    //           this.oSelectedFile = sResult;
-    //           // console.log(sResult)
-    //           // this.oWpPopupDialogRef.componentInstance.patchValue({ 'TABLE_INFO': sResult });
-    //           this.oWpPopupDialogRef.componentInstance.patchValue({ 'TBL_NM': sResult.name });
-    //         }
-    //       }
-    //     });
-
-    //   } else {
-    //     this.resetSelectedForm(this.o_SelectedFormData.CONNECTION_TYPE);
-    //     this.cWpLibSvc.showProgress(true, "wdspin");
-
-    //     this.cMetaSvc.getTableInfo(pDsData['DS_ID']).pipe(
-    //     ).subscribe(pTblData => {
-    //       this.cWpLibSvc.showProgress(false, "wdspin");
-    //       // this.oTblList = pTblData;
-    //       let sTmpFvalue: any = [];
-    //       for (let sIdx of pTblData) {
-    //         sTmpFvalue.push(sIdx['TBL_NM']);
-    //       }
-    //       this.setSelectOption('TABLE_INFO',sTmpFvalue,sTmpFvalue,false)
-
-    //     }, error => {
-    //       this.cWpLibSvc.showProgress(false, "wdspin");
-    //       throw error;
-    //     });
-    //   }
-    // }
   }
 
   addCustomModel() {
@@ -374,26 +251,20 @@ export class CustomManagerComponent implements OnInit {
           this.h_totalFileSize = this.formatBytes(s_result['MODEL_FILE_INFO'].size);
 
           let s_insertResult = await this.cCustomMngSvc.addCustomModel(s_result).toPromise();
-          // let header = {
-          //   "filepath": encodeURIComponent('/'), // 파일 저장 경로
-          //   "token": JSON.parse(localStorage.getItem("currentUser")), // 인증 토큰
-          // }
+
           let s_customId = s_insertResult.result.CUSTOM_ID;
           const s_headers = new HttpHeaders({
-            "filepath": encodeURIComponent(`/custom/${s_customId}/`), // 파일 저장 경로
-            "token": JSON.parse(localStorage.getItem("currentUser")), // 인증 토큰
+            "filepath": encodeURIComponent(`/custom/${s_customId}/`),
+            "token": JSON.parse(localStorage.getItem("currentUser")),
             "personalpath": "Y"
           });
-          // // FileUploadService를 사용하여 파일 업로드
           this.o_uploadSubscription = this.cCustomMngSvc.uploadFile(s_headers, s_result['MODEL_FILE_INFO']).subscribe({
             next: (event) => {
               if (event.type === HttpEventType.UploadProgress) {
-                // 업로드 진행률 계산 및 업데이트
                 this.h_uploadProgress = Math.round((100 * (event.loaded || 0)) / (event.total || 1));
                 this.h_currentFileSize = this.formatBytes(event.loaded || 0);
               } else if (event.type === HttpEventType.Response) {
                 console.log("finish: ");
-                // 업로드 완료 상태 업데이트
                 this.h_uploadProgress = 100;
                 this.h_uploadStatus = '완료';
                 this.h_isUploading = false;
@@ -412,10 +283,6 @@ export class CustomManagerComponent implements OnInit {
                   .then(pResult => {
                       this.getCustomModelList();
                       this.cMainAppSvc.showMsg('커스텀 모델 등록이 완료되었습니다.', true);
-                      // Object.assign(p_event, JSON.parse(pResult)['data']);
-                      // console.log("p_event : ", p_event);
-                      // 상세 페이지 로직
-                      // this.cAppSvc.changeTab('model-profile', p_event);
                   })
                   .catch(error => {
                       console.error("커스텀 모델 등록 중 오류 발생:", error);
@@ -453,7 +320,6 @@ export class CustomManagerComponent implements OnInit {
   showModelPage(p_event: any) {
     console.log(p_event);
     this.cWpLibSvc.showProgress(true, 'algorithmspin');
-    // 실행결과, 파라미터 정보 넘겨서 모델 메트릭스랑 파라미터 정보 가져온다.
     let s_param = {
             method: 'MODEL-INFO',
             location: 'model-manager',
@@ -465,7 +331,6 @@ export class CustomManagerComponent implements OnInit {
 
     this.cCustomMngSvc.getModelInfo(s_param).toPromise().then(pResult => {
         Object.assign(p_event, JSON.parse(pResult)['data']);
-        // 상세 페이지 로직
         this.cAppSvc.changeTab('model-profile', p_event);
     }).catch(error => {
       this.cMainAppSvc.showMsg('모델 정보를 가져오는데 실패하였습니다.', false);
@@ -495,9 +360,6 @@ export class CustomManagerComponent implements OnInit {
     });
     const s_dialogSubmitSub = dialogRef.componentInstance.selectionChanged
       .subscribe(pRes => {
-        // if (pRes.eventNm == "openPythonCode") {
-        //   this.openPythonCode(pRes.selectedVal, dialogRef.componentInstance.oFormcontrol);
-        // }
       });
     let s_dialogSub = dialogRef.afterClosed().subscribe(pRes => {
       s_dialogSubmitSub.unsubscribe();
@@ -533,15 +395,10 @@ export class CustomManagerComponent implements OnInit {
     });
 
   }
-
-
-
-
-  // 업로드 취소 처리
   onCancelUpload(): void {
     if (this.o_uploadSubscription) {
-      this.o_uploadSubscription.unsubscribe(); // 업로드 요청 취소
-    this.h_uploadProgress = 0; // 진행률 초기화
+      this.o_uploadSubscription.unsubscribe();
+    this.h_uploadProgress = 0;
     this.h_uploadStatus = '취소';
     this.h_isUploading = false; 
     }
@@ -549,7 +406,6 @@ export class CustomManagerComponent implements OnInit {
   }
 
 
-  // 바이트 단위 포맷팅
   formatBytes(bytes: number): string {
     if (bytes === 0) return '0B';
     const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
@@ -557,7 +413,6 @@ export class CustomManagerComponent implements OnInit {
     return parseFloat((bytes / Math.pow(1024, i)).toFixed(2)) + sizes[i];
   }
 
-  // 바이트 단위 포맷팅
   selectFrameWork(p_param: any, p_form: any): any {
     console.log("p_param : ", p_param);
     console.log("this.p_form : ", p_form);
@@ -583,8 +438,6 @@ export class CustomManagerComponent implements OnInit {
       dialogRef2.afterClosed().subscribe(pRes => {
       if (pRes && pRes.result) {
           console.log("pRes : ", pRes.code);
-          // 팝업에서 입력한 코드 값.
-          // this.oWpData.pythonCode = pRes.code;
           p_form['MODEL_CLASS_CODE'].setValue(pRes.code);
       }
       });
