f40b3a54fde7c5de4d36cebdcdce6f14ab1e6803
smhan <lion3471@gmail.com>
2025-12-19 14:32:47 +0900

    시계열 컴포넌트 stride 추가

--- projects/wp-ml/job/common/window.py
+++ projects/wp-ml/job/common/window.py
@@ -8,6 +8,8 @@ def execute(p_dataSource, **kwargs):
     s_indexCol = s_data['indexColumn']
     s_windowSize = int(s_data['value'])
     s_targetCol = s_data['column']
+    s_stride = int(s_data['stride'])
+    s_nanhandle = s_data['nanhandle']
 
     s_df = s_df[[s_indexCol, s_targetCol]]
     s_df = s_df.sort_values(by=[s_indexCol])
@@ -15,5 +17,14 @@ def execute(p_dataSource, **kwargs):
     for t in range(1, s_windowSize+1):
         s_df[f't-{t}'] = s_df[s_targetCol].shift(t)
 
+    if s_stride > 0:
+        s_df = s_df.iloc[::s_stride]
+
+    if s_nanhandle == 'remove':
+        s_df.dropna(inplace=True)
+        
+    elif s_nanhandle == 'fill':
+        s_df.fillna(method='ffill', inplace=True)
+        s_df.fillna(method='bfill', inplace=True)
 
     return s_df
